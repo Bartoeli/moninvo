e9eca8c263cb9e8043947538732eaad4acaaaa07
Bara Maresova <bbmaresova@gmail.com>
2020-11-15 20:44:08 +0100

    testing branches 4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React
+          React + Moninvo
+          Bára a Eliška
         </a>
       </header>
     </div>
