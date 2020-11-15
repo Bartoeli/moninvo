65383aee77cf7060fdc55c316ad1471de5217af9
Bara Maresova <bbmaresova@gmail.com>
2020-11-15 21:18:48 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React + Moninvo
+          React
           Bára a Eliška
         </a>
       </header>
