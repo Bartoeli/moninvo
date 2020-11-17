aee3788399516e52850a839db423b62f56f45183
Bara Maresova <bbmaresova@gmail.com>
2020-11-17 21:26:34 +0100

    oprava onClick fce

--- src/components/Header/headerComponents/MenuItem/MenuItem.jsx
+++ src/components/Header/headerComponents/MenuItem/MenuItem.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const MenuItem = (props) => {
   return (
     <li>
-    <a href="#" className="menu-item" onClick={() => props.onSelect()}>
-      {props.text}
-    </a>
+      <a href="#" className="menu-item" onClick={() => props.onSelect()}>
+        {props.text}
+      </a>
     </li>
   );
 };
