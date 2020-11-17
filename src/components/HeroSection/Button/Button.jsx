08d7695abf1377551d236019182ea799c20dc76a
Eliška Bartošová <bartosova.eli@gmail.com>
2020-11-17 21:46:56 +0100

    CTA v hero sekci

---
+++ src/components/HeroSection/Button/Button.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Button = () => {
+  return <button className="cta">VYZKOUŠET</button>;
+};
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
 import { SectionText } from './SectionText/SectionText.jsx';
+import { Button } from './Button/Button.jsx';
 
 export const HeroSection = () => {
   return (
     <>
       <div className="hero-section">
         <SectionText />
+        <Button />
       </div>
     </>
   );
