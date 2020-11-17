9a2b12f6ce3fe2e2d0fa3dafaf34a58286543210
Bara Maresova <bbmaresova@gmail.com>
2020-11-17 13:00:35 +0100

    oprava, složkování v Header comp.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavBar } from './NavBar.jsx';
-import { Profile } from './Profile.jsx';
+import { NavBar } from './headerComponents/NavBar.jsx';
+import { Profile } from './headerComponents/Profile/Profile.jsx';
 import './header.css';
 
 export const Header = () => {
