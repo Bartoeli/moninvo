1bf0fde144e0e54442fc075a39f5728173b79200
Bara Maresova <bbmaresova@gmail.com>
2020-11-18 22:04:04 +0100

    Buttons první návrhy done

--- src/components/Header/headerComponents/Profile/Profile.jsx
+++ src/components/Header/headerComponents/Profile/Profile.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 import './profile.css';
+import { PrimaryBtn } from '../../../Buttons/PrimaryBtn/PrimaryBtn.jsx';
+import { SecondaryBtn } from '../../../Buttons/SecondaryBtn/SecondaryBtn.jsx';
 
 export const Profile = () => {
   const [profileOpened, setProfileOpened] = useState(false);
   return (
     <>
       <button
-        className={
-          profileOpened ? 'profile profile--opened' : 'profile'
-        }
+        className={profileOpened ? 'profile profile--opened' : 'profile'}
         onClick={() => {
           setProfileOpened(!profileOpened);
         }}
       ></button>
-      {profileOpened ? <div className="profileItems">
-        <button className="profileBtn">Přihlásit</button>
-        <button className="profileBtn">Registrovat</button>
-      </div> : null}
+      {profileOpened ? (
+        <div className="profileItems">
+          <SecondaryBtn textBtn="Přihlásit" />
+          <PrimaryBtn textBtn="Registrovat" />
+        </div>
+      ) : null}
     </>
   );
 };
