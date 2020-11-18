15331b6dc179cf2db4bf761b5a8b69f37ac312f7
Bara Maresova <bbmaresova@gmail.com>
2020-11-18 15:08:19 +0100

    přidání hlavního css pro landing page, zakomentování barevné palety, import fontu

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Header } from '../../components/Header/Header.jsx';
 import { HeroSection } from '../../components/HeroSection/HeroSection.jsx';
 import { WhySection } from '../../components/WhySection/WhySection.jsx';
+import './LandingPage.css';
 
-export const LandingPage = ()=>{
+export const LandingPage = () => {
   return (
-  <>
-    <Header />
-    <HeroSection />
-    <WhySection />
-  </>
-  )
-}
+    <>
+      <div className="landingPage">
+        <Header />
+        <HeroSection />
+        <WhySection />
+      </div>
+    </>
+  );
+};
