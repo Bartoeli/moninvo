1fe8023846d98437e1d36fe8ea471506493c6b67
Eliška Bartošová <bartosova.eli@gmail.com>
2020-11-23 00:50:41 +0100

    styly hero sekce na medium device (blob není v pozadí)

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,15 +4,22 @@ import { SectionText } from './SectionText/SectionText.jsx';
 import { PrimaryBtn } from '../Buttons/PrimaryBtn/PrimaryBtn.jsx';
 import { HeroPicture } from './HeroPicture/HeroPicture.jsx';
 import { SecondaryText } from './SecondaryText/SecondaryText.jsx';
+import image from '../../Images/HomePage/hero-01.svg';
 
 export const HeroSection = () => {
   return (
     <>
       <div className="hero-section">
         <HeroPicture />
-        <SectionText />
-        <SecondaryText />
-        <PrimaryBtn className="primary" textBtn="Vyzkoušet" />
+        <div className="left">
+          <SectionText />
+          <SecondaryText />
+          <PrimaryBtn className="primary" textBtn="Vyzkoušet" />
+        </div>
+        <div className="right">
+          <div className="blobik"></div>
+          <img src={image} alt="invoice ilustrace" />
+        </div>
       </div>
     </>
   );
