70f31d20ec9c4f27a07716980065cf5670f75626
Eliška Bartošová <bartosova.eli@gmail.com>
2020-11-22 16:58:06 +0100

    stylování hero section

---
+++ src/components/HeroSection/HeroPicture/HeroPicture.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './heroPicture.css';
+
+export const HeroPicture = () => {
+  return (
+    <>
+      <div className="blob">
+        <img
+          className="ilustration"
+          src="../../../Images/HomePage/hero-01.svg"
+          alt="invoice ilustration"
+        />
+      </div>
+    </>
+  );
+};
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './style.css';
 import { SectionText } from './SectionText/SectionText.jsx';
 import { PrimaryBtn } from '../Buttons/PrimaryBtn/PrimaryBtn.jsx';
+import { HeroPicture } from './HeroPicture/HeroPicture.jsx';
+import { SecondaryText } from './SecondaryText/SecondaryText.jsx';
 
 export const HeroSection = () => {
   return (
     <>
       <div className="hero-section">
+        <HeroPicture />
         <SectionText />
+        <SecondaryText />
         <PrimaryBtn className="primary" textBtn="Vyzkoušet" />
       </div>
     </>
