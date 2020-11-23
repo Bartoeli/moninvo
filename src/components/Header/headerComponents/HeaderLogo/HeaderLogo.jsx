62688ef01cdbf20ed2ceb6a38133b43ec7ad24b8
Bara Maresova <bbmaresova@gmail.com>
2020-11-23 19:34:13 +0100

    úpravy pro větší screeny první část

--- src/components/Header/headerComponents/HeaderLogo/HeaderLogo.jsx
+++ src/components/Header/headerComponents/HeaderLogo/HeaderLogo.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './headerLogo.css';
-import moninvo_mark_WHT from "../../../../Images/Logo/png/moninvo_mark_WHT.png"
+import moninvo_mark_WHT from "../../../../Images/Logo/png/moninvo_mark_WHT.png";
+import moninvo_logo_BLU from "../../../../Images/Logo/png/moninvo_logo_BLU.png";
 
 export const HeaderLogo = () => {
   return (
-    <img className="headerLogo" src={moninvo_mark_WHT} alt="Moninvo logo"/>
+    <span className="logoSpan">
+    <img className="logo_sm" src={moninvo_mark_WHT} alt="Moninvo logo"/>
+    <img className="logo_m" src={moninvo_logo_BLU} alt="Moninvo logo"/>
+    </span>
   )
 }
