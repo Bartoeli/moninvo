0d44ba8126f5bef9405072c7dc2d564ad523bce1
Bara Maresova <bbmaresova@gmail.com>
2020-11-24 21:52:21 +0100

    Why section dokončeny screeny, stylování, ikony, blob pouze na mobil, změna barvy ikon a textu u větších screenů

--- src/components/WhySection/WhySection.jsx
+++ src/components/WhySection/WhySection.jsx
@@ -4,27 +4,50 @@ import { Blob } from '../Blob/Blob.jsx';
 import Dashboard_white from '../../Images/Icons/Dashboard_white.svg';
 import Time_white from '../../Images/Icons/Time_white.svg';
 import Upload_white from '../../Images/Icons/Upload_white.svg';
+import Dashboard_grey from '../../Images/Icons/Dashboard_grey.svg';
+import Time_grey from '../../Images/Icons/Time_grey.svg';
+import Upload_grey from '../../Images/Icons/Upload_grey.svg';
 import './whySection.css';
 
 export const WhySection = () => {
   return (
-    <section className="whySection">
-      <WhyArgument
-        iconSrc={Upload_white}
-        iconAlt="Ikona nahrávání"
-        whyText="Automatické čtení dat z faktur"
-      />
-      <WhyArgument
-        iconSrc={Time_white}
-        iconAlt="Ikona hodin"
-        whyText="Jednoduché ovládání"
-      />
-      <WhyArgument
-        iconSrc={Dashboard_white}
-        iconAlt="Ikona grafu"
-        whyText="Přehledné zobrazení výsledků"
-      />
-      <Blob className="whyBlob" />
-    </section>
+    <>
+      <section className="whySectionMobil">
+        <WhyArgument
+          iconSrc={Upload_white}
+          iconAlt="Ikona nahrávání"
+          whyText="Automatické čtení dat z faktur"
+        />
+        <WhyArgument
+          iconSrc={Time_white}
+          iconAlt="Ikona hodin"
+          whyText="Jednoduché ovládání"
+        />
+        <WhyArgument
+          iconSrc={Dashboard_white}
+          iconAlt="Ikona grafu"
+          whyText="Přehledné zobrazení výsledků"
+        />
+        <Blob className="whyBlob" />
+      </section>
+      <section className="whySectionDesktop">
+        <WhyArgument
+          iconSrc={Upload_grey}
+          iconAlt="Ikona nahrávání"
+          whyText="Automatické čtení dat z faktur"
+        />
+        <WhyArgument
+          iconSrc={Time_grey}
+          iconAlt="Ikona hodin"
+          whyText="Jednoduché ovládání"
+        />
+        <WhyArgument
+          iconSrc={Dashboard_grey}
+          iconAlt="Ikona grafu"
+          whyText="Přehledné zobrazení výsledků"
+        />
+        <Blob className="whyBlob" />
+      </section>
+    </>
   );
 };
