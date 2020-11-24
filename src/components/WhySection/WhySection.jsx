a764b03559b961bdf8c28be1d1ca193babefc65e
Bara Maresova <bbmaresova@gmail.com>
2020-11-24 20:09:40 +0100

    základ hotov

--- src/components/WhySection/WhySection.jsx
+++ src/components/WhySection/WhySection.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { WhyArgument } from './whySectionComponents/WhyArgument.jsx/WhyArgument';
+import Dashboard from '../../Images/Icons/Dashboard.svg';
+import Time from '../../Images/Icons/Time.svg';
+import Upload from '../../Images/Icons/Upload.svg';
+import './whySection.css';
 
 export const WhySection = () => {
-  return(
-  <section>
-    WhySection - prodejní argumenty
-  </section>
-  )
-}
+  return (
+    <section className="whySection">
+      <WhyArgument
+        iconSrc={Upload}
+        iconAlt="Ikona nahrávání"
+        whyText="Automatické čtení dat z faktur"
+      />
+      <WhyArgument
+        iconSrc={Time}
+        iconAlt="Ikona hodin"
+        whyText="Jednoduché ovládání"
+      />
+      <WhyArgument
+        iconSrc={Dashboard}
+        iconAlt="Ikona grafu"
+        whyText="Přehledné zobrazení výsledků"
+      />
+    </section>
+  );
+};
