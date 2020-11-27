a3be1d982467cfe16e94007f10267869cb7a3e69
Bara Maresova <bbmaresova@gmail.com>
2020-11-27 20:13:52 +0100

    dostylování whysection

--- src/components/WhySection/WhySection.jsx
+++ src/components/WhySection/WhySection.jsx
@@ -14,44 +14,28 @@ import './whySection.css';
 
 export const WhySection = () => {
   return (
-    <>
-      <section className="whySectionMobil">
-        <WhyArgument
-          iconSrc={upload}
-          iconAlt="Ikona nahrávání"
-          whyText="Automatické čtení dat z faktur"
-        />
-        <WhyArgument
-          iconSrc={easy}
-          iconAlt="Ikona hodin"
-          whyText="Jednoduché ovládání"
-        />
-        <WhyArgument
-          iconSrc={graph}
-          iconAlt="Ikona grafu"
-          whyText="Přehledné zobrazení výsledků"
-        />
-        <Blob className="whyBlob" />
-      </section>
-      <section className="whySectionDesktop">
-        <WhyArgument
-          className="whyArgument whiteArgument"
-          iconSrc={upload}
-          iconAlt="Ikona nahrávání"
-          whyText="Automatické čtení dat z faktur"
-        />
-        <WhyArgument
-          iconSrc={easy}
-          iconAlt="Ikona hodin"
-          whyText="Jednoduché ovládání"
-        />
-        <WhyArgument
-          iconSrc={graph}
-          iconAlt="Ikona grafu"
-          whyText="Přehledné zobrazení výsledků"
-        />
-        <Blob className="whyBlob" />
-      </section>
-    </>
+    <section className="whySection">
+      <WhyArgument
+        className="whyArgument"
+        iconSrc={upload}
+        iconAlt="Ikona nahrávání"
+        whyText="Automatické čtení dat z faktur"
+        whyInfo="Nahráním faktury získáte automaticky data z pdf a nemusíte ztrácet čas vypisováním."
+      />
+      <WhyArgument
+        iconSrc={easy}
+        iconAlt="Ikona hodin"
+        whyText="Jednoduché ovládání"
+        whyInfo="Uživatelsky příjemné a jednoduché rozhraní s intuitivním ovládáním."
+      />
+      <WhyArgument
+        className="whyArgument"
+        iconSrc={graph}
+        iconAlt="Ikona grafu"
+        whyText="Přehledné zobrazení výsledků"
+        whyInfo="Zobrazte své náklady v přehledných grafech, jejichž zobrazení si můžete přizpůsobit vlastními kategoriemi a štítky."
+      />
+      <Blob className="whyBlob" />
+    </section>
   );
 };
