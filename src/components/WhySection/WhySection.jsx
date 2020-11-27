2957f71c5bdf31cd588676031533712d190c737d
Bara Maresova <bbmaresova@gmail.com>
2020-11-27 18:35:13 +0100

    úpravy z-indexu, nové ikonky

--- src/components/WhySection/WhySection.jsx
+++ src/components/WhySection/WhySection.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { WhyArgument } from './whySectionComponents/WhyArgument.jsx/WhyArgument';
 import { Blob } from '../Blob/Blob.jsx';
-import Dashboard_white from '../../Images/Icons/Dashboard_white.svg';
+/* import Dashboard_white from '../../Images/Icons/Dashboard_white.svg';
 import Time_white from '../../Images/Icons/Time_white.svg';
 import Upload_white from '../../Images/Icons/Upload_white.svg';
 import Dashboard_grey from '../../Images/Icons/Dashboard_grey.svg';
 import Time_grey from '../../Images/Icons/Time_grey.svg';
-import Upload_grey from '../../Images/Icons/Upload_grey.svg';
+import Upload_grey from '../../Images/Icons/Upload_grey.svg'; */
+import easy from '../../Images/Icons/easy.svg';
+import graph from '../../Images/Icons/graph.svg';
+import upload from '../../Images/Icons/upload.svg';
 import './whySection.css';
 
 export const WhySection = () => {
@@ -14,17 +17,17 @@ export const WhySection = () => {
     <>
       <section className="whySectionMobil">
         <WhyArgument
-          iconSrc={Upload_white}
+          iconSrc={upload}
           iconAlt="Ikona nahrávání"
           whyText="Automatické čtení dat z faktur"
         />
         <WhyArgument
-          iconSrc={Time_white}
+          iconSrc={easy}
           iconAlt="Ikona hodin"
           whyText="Jednoduché ovládání"
         />
         <WhyArgument
-          iconSrc={Dashboard_white}
+          iconSrc={graph}
           iconAlt="Ikona grafu"
           whyText="Přehledné zobrazení výsledků"
         />
@@ -32,18 +35,18 @@ export const WhySection = () => {
       </section>
       <section className="whySectionDesktop">
         <WhyArgument
-        className="whyArgument whiteArgument"
-          iconSrc={Upload_grey}
+          className="whyArgument whiteArgument"
+          iconSrc={upload}
           iconAlt="Ikona nahrávání"
           whyText="Automatické čtení dat z faktur"
         />
         <WhyArgument
-          iconSrc={Time_grey}
+          iconSrc={easy}
           iconAlt="Ikona hodin"
           whyText="Jednoduché ovládání"
         />
         <WhyArgument
-          iconSrc={Dashboard_grey}
+          iconSrc={graph}
           iconAlt="Ikona grafu"
           whyText="Přehledné zobrazení výsledků"
         />
