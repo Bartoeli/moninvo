e2a4277f0a4bc8cbfbb81305f2932ad7a4d89e7a
Bara Maresova <bbmaresova@gmail.com>
2020-11-28 15:22:10 +0100

    opravené responzivní logo

--- src/components/Header/HeaderDash.jsx
+++ src/components/Header/HeaderDash.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { HeaderLogo } from './headerComponents/HeaderLogo/HeaderLogo';
+import moninvo_logo_WHT from '../../Images/Logo/png/moninvo_logo_WHT.png';
 import './headerDash.css';
 
 export const HeaderDash = () => {
   return (
     <header className="headerDashboard">
-      <HeaderLogo />
+      <img className="logo_wht_dash" src={moninvo_logo_WHT} alt="Moninvo logo" />
     </header>
   );
 };
