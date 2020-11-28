c4e9fbaaf2299bcb8a3c74613942fdf612ee1d2e
Bara Maresova <bbmaresova@gmail.com>
2020-11-28 15:44:49 +0100

    smazání ExportRossum komp, přidání jejího obsahu do Dashboardu

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRossum } from '../../utils/Rossum/Rossum.jsx';
 import { HeaderDash } from '../../components/Header/HeaderDash.jsx';
-import { ExportRossum } from '../../utils/Rossum/ExportRossum.jsx';
+import { ExportRossum } from './ExportRossum.jsx';
 
 export const Dashboard = () => {
-  console.log(useRossum());
+  const result = useRossum().token;
+
+  useEffect(() => {
+    fetch(
+      'https://api.elis.rossum.ai/v1/queues/71919/export?format=json&status=exported',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `token ${result}`,
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <>
       <HeaderDash />
