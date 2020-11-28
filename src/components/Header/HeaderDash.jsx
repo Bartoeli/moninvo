4d6a3f787758e3c61aa760e1dfa211219026ea05
Bara Maresova <bbmaresova@gmail.com>
2020-11-28 13:13:58 +0100

    header dashboard

---
+++ src/components/Header/HeaderDash.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HeaderLogo } from './headerComponents/HeaderLogo/HeaderLogo';
+import './headerDash.css';
+
+export const HeaderDash = () => {
+  return (
+    <header className="headerDashboard">
+      <HeaderLogo />
+    </header>
+  );
+};
