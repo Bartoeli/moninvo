eed32d822c6749f3f854dd63692a279924a3742e
Eliška Bartošová <bartosova.eli@gmail.com>
2020-11-29 10:39:54 +0100

    import knihovny na tabulky a grafy

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import './style.css';
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
