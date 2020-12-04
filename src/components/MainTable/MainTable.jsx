e2ad6753261976f44b6527e828f6f86b3848fd48
Bara Maresova <bbmaresova@gmail.com>
2020-12-04 21:17:12 +0100

    responsivní tabulka začátek

--- src/components/MainTable/MainTable.jsx
+++ src/components/MainTable/MainTable.jsx
@@ -4,6 +4,7 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import './mainTable.css';
 
 export const MainTable = (props) => {
   const paginatorLeft = (
@@ -15,22 +16,23 @@ export const MainTable = (props) => {
 
   return (
     <>
-      <DataTable
-        value={props.data}
-        className="p-datatable-striped p-datatable-responsive-demo"
-        paginator
-        paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
-        currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
-        rows={5}
-        rowsPerPageOptions={[5, 10, 20, 50]}
-        paginatorLeft={paginatorLeft}
-        paginatorRight={paginatorRight}
-        sortMode="multiple"
-        header="Moje nahrané faktury"
-        scrollable
-        scrollHeight="200px"
-      >
-        {/* <span className="p-input-icon-left">
+      <div className="datatable-responsive">
+        <DataTable
+          value={props.data}
+          className="p-datatable-striped p-datatable-responsive"
+          paginator
+          paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+          rows={6}
+          rowsPerPageOptions={[6, 10, 20, 50]}
+          paginatorLeft={paginatorLeft}
+          paginatorRight={paginatorRight}
+          sortMode="multiple"
+          header="Moje nahrané faktury"
+          scrollable
+          scrollHeight="200px"
+        >
+          {/* <span className="p-input-icon-left">
           <i className="pi pi-search" />
           <InputText
             type="search"
@@ -38,18 +40,19 @@ export const MainTable = (props) => {
             placeholder="Global Search"
           />
         </span> */}
-        <Column field="sender_name" header="Dodavatel" sortable></Column>
-        <Column field="invoice_id" header="Číslo faktury" sortable></Column>
-        <Column
-          field="amount_total_base"
-          header="Částka bez DPH"
-          sortable
-        ></Column>
-        <Column field="amount_total" header="Částka s DPH" sortable></Column>
-        <Column field="currency" header="Měna" sortable></Column>
-        <Column field="date_issue" header="Vystaveno" sortable></Column>
-        <Column field="Note" header="Poznámka" sortable></Column>
-      </DataTable>
+          <Column field="sender_name" header="Dodavatel" sortable></Column>
+          <Column field="invoice_id" header="Číslo faktury" sortable></Column>
+          <Column
+            field="amount_total_base"
+            header="Částka bez DPH"
+            sortable
+          ></Column>
+          <Column field="amount_total" header="Částka s DPH" sortable></Column>
+          <Column field="currency" header="Měna" sortable></Column>
+          <Column field="date_issue" header="Vystaveno" sortable></Column>
+          <Column field="Note" header="Poznámka" sortable></Column>
+        </DataTable>
+      </div>
     </>
   );
 };
