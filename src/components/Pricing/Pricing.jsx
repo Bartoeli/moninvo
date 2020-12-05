471f9ab6212545d4c4fdd93dddda7e3bffc3b448
Bara Maresova <bbmaresova@gmail.com>
2020-12-05 14:06:00 +0100

    responsivní ceník se scrollováním done

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,29 +1,102 @@
 import React from 'react';
 import './pricing.css';
-import { PricingModel } from './PricingComponents/PricingModel';
+import { PricingModelFunction } from './PricingComponents/PricingModelFunction.jsx';
+import checked from '../../Images/Icons/checked.svg';
+import unchecked from '../../Images/Icons/unchecked.svg';
 
 export const Pricing = () => {
   return (
-    <div className="pricingComp">
+    <div className="pricing">
       <h2>Ceník</h2>
-      <div className="pricingModels">
-        <PricingModel
-          modelName="Donald"
-          price="150Kč"
-          priceDetail="měsíční platba"
 
-        />
-        <PricingModel
-          modelName="McQuack"
-          price="300 Kč"
-          priceDetail="měsíční platba"
-          classNameModel="backgroundModel"
-        />
-        <PricingModel
-          modelName="Scrooge"
-          price="600 Kč"
-          priceDetail="měsíční platba"
-        />
+      <div className="pricingModels">
+        <div className="pricingModel">
+          <h3>Nejdřív zkouším, potom platím</h3>
+          <div className="priceDetails">
+            <h4>Zdarma</h4>
+            <h5>Maximální objem: 5 faktur měsíčně</h5>
+          </div>
+          <div className="pricingModelFunctions">
+            <PricingModelFunction
+              functionName="Omezené nahrávání faktur (5 měsíčně)"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Zobrazení jednoho grafu"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Vlastní kategorie a štítky v grafu"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Export dat do Excelu"
+              check={unchecked}
+            />
+            <PricingModelFunction
+              functionName="Telefonická podpora"
+              check={unchecked}
+            />
+          </div>
+        </div>
+        <div className="pricingModel backgroundModel">
+          <h3>Spokojím se s málem</h3>
+          <div className="priceDetails">
+            <h4>1500Kč/měsíc</h4>
+            <h5>Maximální objem: 50 faktur měsíčně.</h5>
+          </div>
+          <div className="pricingModelFunctions">
+            <PricingModelFunction
+              functionName="Omezené nahrávání faktur (50 měsíčně)"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Zobrazení tří grafů"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Vlastní kategorie a štítky v grafu"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Export dat do Excelu"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Telefonická podpora 24/7"
+              check={unchecked}
+            />
+          </div>
+        </div>
+        <div className="pricingModel">
+          <h3>Chci to všechno</h3>
+          <div className="priceDetails">
+            <h4>Cena na vyžádání</h4>
+            <h5>Rádi vám sestavíme cenový plán na míru.</h5>
+          </div>
+          <div className="pricingModelFunctions">
+            <PricingModelFunction
+              functionName="Neomezené nahrávání faktur"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Neomezený počet grafů"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Vlastní kategorie a štítky v grafu"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Export dat"
+              check={checked}
+            />
+            <PricingModelFunction
+              functionName="Telefonická podpora 24/7"
+              check={checked}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
